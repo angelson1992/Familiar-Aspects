aeeca8c0a5aab4b5c8cbb20a2cf188d064ac6b5c
John <Angelson1992@gmail.com>
2016-09-08 11:09:23 -0400

    Testing new Blocks

--- src/main/java/com/carryncare/FamiliarAspects/FamiliarAspects.java
+++ src/main/java/com/carryncare/FamiliarAspects/FamiliarAspects.java
@@ -1,20 +1,30 @@
 package com.carryncare.FamiliarAspects;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = FamiliarAspects.MODID, version = FamiliarAspects.VERSION)
 public class FamiliarAspects
 {
     public static final String MODID = "FamiliarAspects";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+
+        Block myBlock = new Block(Material.CLOTH).setLightLevel(8).setHardness(0.25f).setCreativeTab(CreativeTabs.DECORATIONS).setUnlocalizedName("Hello World");
+        GameRegistry.register(myBlock.setRegistryName("Hello World"));
+        GameRegistry.register(new ItemBlock(myBlock).setRegistryName(myBlock.getRegistryName()));
+
     }
 }
